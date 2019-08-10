34b8de1c96ec530e5c2cce169660ec088ac76e89
Deb <49885983+dawarrick@users.noreply.github.com>
2019-08-10 19:21:47 -0400

    add te supervisor module

--- README.md
+++ README.md
@@ -1,28 +1,54 @@
-# bamazonCustomer.js - buy your items from this CLI interface
 
+# bamazon!  Multiple features to an on-line order system
 #### Developed by Deb Warrick https://debwarrick.com
 
-* bamazon is an example of my first use of MySQL, in conjunction with NODE and inquirer to provide a user interface
+* bamazon is an example of using MySQL, in conjunction with NODE and inquirer to provide a user interface
 * bamazon can't be run from a URL, it must be executed using Node.js.
+
+## bamazonCustomer.js - buy your items from this CLI interface
+## bamazonManager.js - will allow you to:  view products for sale, see those below inventory limits (5), add quantity to inventory, add a new product
+## bamazonSupervisor.js - will allow you to run a net profit report, and add a new department.
+
 
 ## How it works
 
+### NODE bamazonCustomer
+
 You will be shown a list of the products in the bamazon database, including the price and the quantity available.
 You will enter the item number, and then the quantity you wish to purchase.
-You can purchase multiple items in one order.  When you are finished with your order, simply hit enter and your order will be compete.
+You can purchase multiple items in one order.  When you are finished with your order, simply hit enter and your order will be complete.
 If you have started an order and wish to cancel, type in Q.  If you order more than we have in stock, we'll let you know so you can adjust.
 You will be given an order total at the end.
-
-**To execute the purchase process type NODE bamazonCustomer.
   
-## Additional Features
+##### Watch me walk you through it.
+![](bamazonCustomer.gif)
 
-#### All entries are case insensitive.
+
+### NODE bamazonManager
+
+You will be given a menu to select from.  Use the up and down arrows to navigate through.
+
+##### View Products for Sale - will display our current inventory
+##### View Low Inventory - will list the item with a in-stock inventory of less than 5 so you know when to reorder.
+##### Add to Inventory - will allow you to log the receipt of additional quantity of existing items into inventory.  Enter as many as you have and then complete
+##### Add New Product - will allow you to add new products to our inventory
+
 
 ##### Watch me walk you through it.
-![](spotify-this-song.gif)
+![](bamazonManager.gif)
 
 
+
+### NODE bamazonSupervisor
+
+You will be given a menu to select from.  Use the up and down arrows to navigate through.
+
+##### View Product Sales by Department - will give you a summary report of the profit by department based on the overhead.
+##### "Create New Department - will allow you to create a new deparment for item entries.
+
+
+##### Watch me walk you through it.
+![](bamazonSupervisor.gif)
 
 ### bamazon uses the following Node.js libraries that you must install.
 
---
+++ bamazonSupervisor.js
@@ -0,0 +1,136 @@
+//get the environment variable
+require("dotenv").config();
+
+//load up the includes
+var keys = require("./keys.js");
+var mysql = require("mysql");
+
+var inquirer = require("inquirer");
+const cTable = require('console.table');
+var mysqlPassword = keys.mysql;
+var departments = [];
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,// Your port; if not 3306
+    user: "root",  // Your username
+    password: mysqlPassword.password,  // Your password
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    loadDepartments();
+    menuOptions();
+});
+
+//departments for adding a new item
+function loadDepartments() {
+    console.clear();
+
+    var query = "SELECT department_name FROM departments ORDER BY department_name";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+
+        res.forEach(function (ea) {
+            departments.push(
+                ea.department_name
+            );
+        })
+    });
+}
+
+//load the items from the database into an array for processing.
+//limit - 0 means load and display all. -1 means load all and display and return to calling program, any ohter number means show those with qty < limit.
+function viewProductSales() {
+    console.clear();
+    results = [];
+    response = [];
+    var query = "SELECT d.id, d.department_name, d.over_head_costs, SUM(IFNULL(p.product_sales,0)) product_sales, SUM(IFNULL(p.product_sales,0))-IFNULL(d.over_head_costs,0) total_profit"
+    query += " FROM departments d LEFT JOIN products p ON d.department_name = p.department_name GROUP BY d.id, d.department_name, d.over_head_costs";
+
+    connection.query(query, function (err, res) {
+
+        if (err) throw err;
+        console.table(res)
+        //go back to the menu
+        menuOptions();
+    });
+}
+
+
+//will validate what was passed in, and optionally prompt the user if they passed in crap
+function menuOptions() {
+    // If user didn't enter a command, or entered an invalid one, prompt for one
+
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "doingWhat",
+            message: "Select what you would like to do",
+            choices: ["View Product Sales by Department", "Create New Department", "Quit"]
+        },
+
+    ]).then(function (input) {
+
+        //get a random selection from those previously
+        if (input.doingWhat === "View Product Sales by Department") {
+            viewProductSales();
+        }
+        // convert selection to command
+        else if (input.doingWhat === "Create New Department") {
+            createDepartment();
+        }
+        //quit
+        else {
+            process.exit(1);
+        }
+    });
+
+}
+
+
+//these functions are for adding a new item
+//will prompt for product name, department, price, quantity
+function createDepartment() {
+
+    inquirer.prompt([
+        {
+            type: "item",
+            name: "department",
+            message: "Enter Department Name"
+        },
+        {
+            type: "input",
+            name: "costs",
+            message: "Enter Overhead Costs"
+        },
+
+    ]).then(function (input) {
+        //they've added a department
+        //make sure valid entry
+        if (input.department === "" || parseFloat(input.costs) == 0) {
+            console.log("All data fields are required");
+        }
+        else {
+            addToDB(input.department, parseFloat(input.costs));
+        }
+        menuOptions();
+    });
+
+}
+
+//once the item has been validated, update the inventory
+function addToDB(department, costs) {
+    connection.query(
+        "INSERT INTO departments SET ?",
+        {
+            department_name: department,
+            over_head_costs: costs
+        },
+        function (err, res) {
+            if (err) throw err;
+
+        }
+    );
+}
